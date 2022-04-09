332eb0726eec23d2adbc7a726a622353b5a93da4
Rob Stringer <funkmunk@gmail.com>
2022-04-09 22:01:33 +0200

    Text styles implemented. Some colour scheme playing around

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,11 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import Typography from 'typography';
+import lincolnTheme from 'typography-theme-lincoln';
+
+// moragaTheme.baseFontSize = '16px';
+const typography = new Typography(lincolnTheme);
+typography.injectStyles();
 
 export default class ApplicationRoute extends Route {
   @service store;
@@ -7,7 +13,6 @@ export default class ApplicationRoute extends Route {
   async model() {
     let response = await fetch('/api/index.json');
     let { role } = await response.json();
-    console.log({ role });
     this.store.push({ data: role });
     return role;
   }
